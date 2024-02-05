78927e540753618b5c870002da28db2ff00e8420
fanie <faniejohnsons98@gmail.com>
2024-02-05 12:42:57 +0200

    menu front side

--- express.js
+++ express.js
@@ -1,9 +1,34 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require("body-parser");
+const session = require("express-session");
 const fs = require('fs');
 const yaml = require('js-yaml');
 
 const app = express();
 const port = 3000;
+
+//Configuring Handlebars
+const handlebarSetup = exphbs.engine({
+    partialsDir: "./views/partials",
+    viewPath: './views',
+    layoutsDir: './views/layouts'
+});
+app.engine('handlebars', handlebarSetup);
+app.set('view engine', 'handlebars');
+app.use(express.static("public"));
+
+//Body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Session
+app.use(session({
+    secret: 'codeforgeek',
+    resave: true,
+    saveUninitialized: true
+}));
+
 
 // Function to read YAML file and convert it to JSON
 const readYamlAndConvertToJson = (yamlFilePath) => {
@@ -13,10 +38,11 @@ const readYamlAndConvertToJson = (yamlFilePath) => {
 };
 
 // Route to serve the JSON representation of the YAML file
-app.get('/menu', (req, res) => {
-  const yamlFilePath = './foodMood.yaml'; // Update with the actual path to your YAML file
-  const jsonData = readYamlAndConvertToJson(yamlFilePath);
-  res.json(JSON.parse(jsonData));
+app.get('/', (req, res) => {
+    const yamlFilePath = './foodMood.yaml'; // Update with the actual path to your YAML file
+    const data = readYamlAndConvertToJson(yamlFilePath);
+    console.log(data)
+    res.render('menu', { data });
 });
 
 
