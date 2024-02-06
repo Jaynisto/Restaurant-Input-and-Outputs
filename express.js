e72a71ee74db2a1cd89faf446b496df95e7ffe9e
fanie <faniejohnsons98@gmail.com>
2024-02-06 14:03:59 +0200

    set env port

--- express.js
+++ express.js
@@ -46,6 +46,7 @@ app.get('/', (req, res) => {
 
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+const PORT = process.env.PORT || 5050;
+app.listen(PORT, (req, res) => {
+    console.log("Application Fired On " + PORT + "!")
 });
